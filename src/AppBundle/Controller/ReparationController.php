fbfbe5cb3852047dd499e485e8de21da73f6b766
Martín Peveri <martinpeveri@gmail.com>
2017-02-19 00:04:32 -0300

    Improvements in the generation of pdf

--- src/AppBundle/Controller/ReparationController.php
+++ src/AppBundle/Controller/ReparationController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReparationController extends Controller
 {
@@ -13,17 +14,17 @@ class ReparationController extends Controller
      */
     public function printAction(Request $request)
     {
-        header("Content-type:application/pdf");
-        header("Content-Disposition:attachment;filename='reparation.pdf'");
-
-        $em = $this->getDoctrine()->getManager();
         $reparation = $this->getDoctrine()->getRepository('AppBundle:Reparation');
 
         $id = $request->query->get('id');
         $record = $reparation->find($id);
 
-        return $this->render('reparation/index.html.twig', array(
+        $html = $this->renderView('reparation/index.html.twig', array(
             'record' => $record,
         ));
+
+        $mpdf = new \mPDF();
+        $mpdf->WriteHTML($html);
+        return new Response($mpdf->Output('reparation.pdf', 'I'));
     }
 }
